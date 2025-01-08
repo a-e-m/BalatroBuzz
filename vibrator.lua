b034a8495d3d11a16b307891566d7b77bf642cd5
a-e-m <amyemartyn@gmail.com>
2025-01-08 09:50:36 -0800

    Support for automatically determing Lovense host

--- vibrator.lua
+++ vibrator.lua
@@ -1,20 +1,41 @@
 function update_vibrator(strength)
-    if not G.VIBRATION_THREAD then
+    if not G.VIBRATION_MANAGER then
         -- We perform the requests on a separate thread to avoid causing UI lag
-        G.VIBRATION_THREAD = love.thread.newThread([[
+        local vibrationThread = love.thread.newThread([[
             local https = require('https')
-            CHANNEL = love.thread.getChannel("vibration_channel")
+
+            function get_lovense_host()
+                local code, body = https.request('https://api.lovense.com/api/lan/getToys')
+                -- weirdly, the response seems to not be valid JSON, so we do janky string search
+                local i, j = string.find(body, '".-[.]lovense[.]club')
+                if (i) then
+                    local host = body.sub(body, i + 1, j)
+                    return host
+                end
+            end
+
+            CHANNEL_IN = love.thread.getChannel("vibration_channel_in")
+            CHANNEL_OUT = love.thread.getChannel("vibration_channel_out")
+
+            local host = get_lovense_host() or "{{lovely:vibrate_host}}"
+            local command_url = "http://"..host..":".."{{lovely:vibrate_port}}".."/command"
+
+            CHANNEL_OUT:push(command_url)
 
             while true do
                 --Monitor the channel for any new requests
-                local data = CHANNEL:demand()
+                local data = CHANNEL_IN:demand()
                 if data then
-                    https.request('{{lovely:vibrate_url}}', { headers = {["Content-Type"] = "application/json"}, data = data, method = "POST" })
+                    https.request(command_url, { headers = {["Content-Type"] = "application/json"}, data = data, method = "POST" })
                 end
             end
         ]])
-        G.VIBRATION_THREAD:start()
-        G.VIBRATION_CHANNEL = love.thread.getChannel('vibration_channel')
+        G.VIBRATION_MANAGER = {
+            thread = vibrationThread,
+            in_channel = love.thread.getChannel('vibration_channel_in'),
+            out_channel = love.thread.getChannel("vibration_channel_out"),
+        }
+        vibrationThread:start()
     end
 
     G.LAST_VIBRATE = G.LAST_VIBRATE or 0
@@ -31,7 +52,7 @@ function update_vibrator(strength)
         local data = [[{{lovely:vibrate_payload}}]]
         data = data:gsub('$intensity', scaledStrength)
 
-        G.VIBRATION_CHANNEL:push(data)
+        G.VIBRATION_MANAGER.in_channel:push(data)
         G.LAST_VIBRATE = love.timer.getTime()
     end
 end
